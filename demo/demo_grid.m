6825dd371071c8684a78bdf52aee71d7ad632836
marionmari <marion.neumann@iais.fraunhofer.de>
2014-06-03 19:03:20 +0200

    CV commented out

--- demo/demo_grid.m
+++ demo/demo_grid.m
@@ -28,29 +28,29 @@ K = propagation_kernel_matrix(inital_A, transformation, num_iter, 'distance', di
 toc                     
       
 
-% KERNEL EVALUATION (libSVM Classification)
-fprintf('...svm evaluation\n')
-addpath(genpath('/home/marion/MATLAB/libsvm-3.17/'))
-svm_options = @(c)(['-q -t 4 -c ' num2str(c)]);
-
-num_folds = 10; 
-cost = 1;
-c = cvpartition(numel(labels),'kfold',num_folds);  % generate data splits
-
-accurracies = zeros(num_folds, 1);
-for i=1:num_folds
-    train_ind = find(training(c,i)==1);
-    test_ind = find(training(c,i)==0);  
-
-    K_train = [(1:length(train_ind))' K(train_ind,train_ind)];
-    K_test = [(1:length(test_ind))' K(test_ind,train_ind)];
-
-    % SVM prediciton
-    model = svmtrain_libsvm(labels(train_ind),K_train, svm_options(cost));
-    [y_pred, acc, ~] = svmpredict(labels(test_ind),K_test, model, '-q');
-    accurracies(i) = acc(1);
-end
-fprintf('%d-fold CV accuracy (+/- stdv) = %2.2f (+/- %0.2f) \n',num_folds,mean(accurracies),std(accurracies))                     
+% % KERNEL EVALUATION (libSVM Classification)
+% fprintf('...svm evaluation\n')
+% addpath(genpath('/path/to/libsvm/'))
+% svm_options = @(c)(['-q -t 4 -c ' num2str(c)]);
+% 
+% num_folds = 10; 
+% cost = 1;
+% c = cvpartition(numel(labels),'kfold',num_folds);  % generate data splits
+% 
+% accurracies = zeros(num_folds, 1);
+% for i=1:num_folds
+%     train_ind = find(training(c,i)==1);
+%     test_ind = find(training(c,i)==0);  
+% 
+%     K_train = [(1:length(train_ind))' K(train_ind,train_ind)];
+%     K_test = [(1:length(test_ind))' K(test_ind,train_ind)];
+% 
+%     % SVM prediciton
+%     model = svmtrain_libsvm(labels(train_ind),K_train, svm_options(cost));
+%     [y_pred, acc, ~] = svmpredict(labels(test_ind),K_test, model, '-q');
+%     accurracies(i) = acc(1);
+% end
+% fprintf('%d-fold CV accuracy (+/- stdv) = %2.2f (+/- %0.2f) \n',num_folds,mean(accurracies),std(accurracies))                     
 
 
 
